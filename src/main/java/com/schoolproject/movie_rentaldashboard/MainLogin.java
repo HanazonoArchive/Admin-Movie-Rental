d69e2642b04a388dd9097a18388e95949a1e0770
Mark Jade Palma <jedzxcx1@gmail.com>
2024-02-28 03:29:02 +0800

    Added LoginPages

---
+++ src/main/java/com/schoolproject/movie_rentaldashboard/MainLogin.java
@@ -0,0 +1,24 @@
+
+package com.schoolproject.movie_rentaldashboard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+
+public class MainLogin extends Application {
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	LoginDemo demo = new LoginDemo(primaryStage);
+	//AdminLogin admin = new AdminLogin(primaryStage);
+	//new SignUp(primaryStage);
+	
+	}
+}
