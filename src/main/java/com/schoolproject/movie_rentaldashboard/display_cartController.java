3848dd60c620aaa6f3b26935b40d29efb51dde9d
Mark Jade Palma <jedzxcx1@gmail.com>
2024-03-02 22:10:32 +0800

    demo on login and db

--- src/main/java/com/schoolproject/movie_rentaldashboard/display_cartController.java
+++ src/main/java/com/schoolproject/movie_rentaldashboard/display_cartController.java
@@ -1,14 +1,27 @@
 package com.schoolproject.movie_rentaldashboard;
 
+import com.schoolproject.database.UserFunctions;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 public class display_cartController {
 
     @FXML
     private AnchorPane cart_screen;
+    //@FXML
+    //private TableView<?> orderTable;
 
     public void setHomeDisplay_Cart(AnchorPane homeDisplay) {
         homeDisplay.getChildren().setAll(cart_screen);
     }
+
+    /*public void setInformation(){
+        UserFunctions functions = new UserFunctions();
+        //functions.userData("");
+        orderTable.setAccessibleText(functions.userData(""));
+
+    }*/
+
+
 }
