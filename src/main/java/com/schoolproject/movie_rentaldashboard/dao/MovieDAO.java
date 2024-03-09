953af5785510e7f18aee47f43417d973d98329b4
Andrew <drewstackdev@gmail.com>
2024-03-10 02:52:10 +0800

    backup before major setting change

--- src/main/java/com/schoolproject/movie_rentaldashboard/dao/MovieDAO.java
+++ src/main/java/com/schoolproject/movie_rentaldashboard/dao/MovieDAO.java
@@ -2,9 +2,17 @@ package com.schoolproject.movie_rentaldashboard.dao;
 
 import com.schoolproject.movie_rentaldashboard.model.Movie;
 
+import java.util.List;
+
 public interface MovieDAO {
     Movie getMovieById(String movieId);
+
+    List<Movie> getAllMovies();
+
     boolean addMovie(Movie movie);
+
+    boolean deleteMovie(String movieId);
+
     boolean updateMovieDetails(Movie movie);
     // Add other methods as needed
 }
--- src/main/java/com/schoolproject/movie_rentaldashboard/dao/mysql/MySQLMovieDAO.java
+++ src/main/java/com/schoolproject/movie_rentaldashboard/dao/mysql/MySQLMovieDAO.java
@@ -3,20 +3,21 @@ package com.schoolproject.movie_rentaldashboard.dao.mysql;
 import com.schoolproject.movie_rentaldashboard.dao.MovieDAO;
 import com.schoolproject.movie_rentaldashboard.model.Movie;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.schoolproject.movie_rentaldashboard.dao.mysql.helper.MoveCastConversionHelper.convertListToString;
 import static com.schoolproject.movie_rentaldashboard.dao.mysql.helper.MoveCastConversionHelper.convertStringToList;
 
 public class MySQLMovieDAO implements MovieDAO {
-
+    public static final String SEARCH_MOVIES_QUERY = "SELECT * FROM Movies WHERE title LIKE ? OR genre LIKE ?";
+    private static final String SELECT_ALL_MOVIES_QUERY = "SELECT * FROM Movies";
+    public static final String DELETE_MOVIE_QUERY = "DELETE FROM Movies WHERE movieId=?";
     private static final String GET_MOVIE_BY_ID_QUERY = "SELECT * FROM Movies WHERE movieId=?";
     private static final String ADD_MOVIE_QUERY = "INSERT INTO Movies (title, cast, genre, duration, ageRating, description, image, price, year) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     private static final String UPDATE_MOVIE_DETAILS_QUERY = "UPDATE Movies SET title=?, cast=?, genre=?, duration=?, ageRating=?, description=?, image=?, price=?, year=? WHERE movieId=?";
+
 
     @Override
     public Movie getMovieById(String movieId) {
@@ -40,7 +41,6 @@ public class MySQLMovieDAO implements MovieDAO {
                     boolean available = resultSet.getBoolean("available");
                     int stockQuantity = resultSet.getInt("stockQuantity");
 
-//                    return new Movie(movieId, title, cast, genre, duration, ageRating, description, image, price, year);
                     return new Movie(movieId, title, cast, genre, duration, ageRating, description, image, price, year, averageRating, totalRatings, available, stockQuantity);
                 }
 
@@ -50,6 +50,45 @@ public class MySQLMovieDAO implements MovieDAO {
         }
         return null;
     }
+
+
+    @Override
+    public List<Movie> getAllMovies() {
+        List<Movie> movies = new ArrayList<>();
+        try (Connection connection = MySQLDBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_MOVIES_QUERY)) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while (resultSet.next()){
+
+                }
+
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    private Movie extractMovieFromResultSet(ResultSet resultSet) throws SQLException {
+        Movie movie = new Movie();
+        movie.resultSet.getInt("movieId");
+        resultSet.getString("title");
+        resultSet.getString("cast");
+        resultSet.getString("genre");
+        resultSet.getInt("duration");
+        resultSet.getString("ageRating");
+        resultSet.getString("description");
+        resultSet.getString("image");
+        resultSet.getDouble("price");
+        resultSet.getInt("year");
+        resultSet.getDouble("averageRating");
+        resultSet.getInt("totalRatings");
+        resultSet.getBoolean("available");
+        resultSet.getInt("stockQuantity");
+    }
+
 
     @Override
     public boolean addMovie(Movie movie) {
@@ -66,6 +105,43 @@ public class MySQLMovieDAO implements MovieDAO {
             preparedStatement.setDouble(8, movie.getPrice());
             preparedStatement.setInt(9, movie.getYear());
 
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public List<Movie> searchMovies(String keyword) {
+        List<Movie> movies = new ArrayList<>();
+        try (Connection connection = MySQLDBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SEARCH_MOVIES_QUERY)) {
+
+            String searchKeyword = "%" + keyword + "%";
+            preparedStatement.setString(1, searchKeyword);
+            preparedStatement.setString(2, searchKeyword);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    // Extract movie details and create Movie objects
+                    Movie movie = extractMovieFromResultSet(resultSet);
+                    movies.add(movie);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+
+    @Override
+    public boolean deleteMovie(String movieId) {
+        try (Connection connection = MySQLDBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_MOVIE_QUERY)) {
+
+            preparedStatement.setInt(1, Integer.parseInt(movieId, 10));
             return preparedStatement.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -95,4 +171,6 @@ public class MySQLMovieDAO implements MovieDAO {
         }
         return false;
     }
+
+
 }
